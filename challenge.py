8ea2ca43089670bf006abde41b29f13305f5e0ae
Kevin <99064091@mydavinci.nl>
2021-09-15 09:47:37 +0200

    update

---
+++ challenge.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+nummer = int(input('voer een nummer in')
+
+print(nummer)
+
+if int('voer een nummer in') = int:
+    print(type(nummer)
+else:
+    print(type("geen nummer")
